6636ff4fc4623d0a0a06542080ff2148ac4d241e
IA-n7 <ian.e.w.bradley@gmail.com>
2018-06-26 13:51:55 +0000

    Modified given code to display information base one command line arguement, Postgres Pratice

---
+++ test_script.js
@@ -0,0 +1,37 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+const name = process.argv[2];
+
+function displayResult(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    count++;
+    let firstName = arr[i]["first_name"];
+    let lastName = arr[i]["last_name"]; 
+    let birthdate = arr[i]["birthdate"];
+    console.log(` - ${count}: ${firstName} ${lastName}, born ${birthdate}`);
+  }
+}
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people AS name WHERE first_name = '${name}' OR last_name = '${name}'`, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    displayResult(result.rows);
+    client.end();
+  });
+});
