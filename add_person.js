f974923d46c12f01905b385ddef5e63c0275bc88
IA-n7 <ian.e.w.bradley@gmail.com>
2018-06-26 17:05:00 +0000

    Finished Postgres practice

---
+++ add_person.js
@@ -0,0 +1,22 @@
+const settings = require("./settings"); // settings.json
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+const birthDate = process.argv[4];
+
+knex.insert([{first_name: firstName, last_name: lastName,  birthdate: birthDate}]).into('famous_people')
+.then(function (id) {})
+.finally(function() {
+  knex.destroy();
+});
---
+++ knex_script.js
@@ -0,0 +1,31 @@
+const settings = require("./settings"); // settings.json
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+
+const name = process.argv[2];
+
+function displayResult(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    count++;
+    let firstName = arr[i]["first_name"];
+    let lastName = arr[i]["last_name"]; 
+    let birthdate = arr[i]["birthdate"];
+    console.log(` - ${count}: ${firstName} ${lastName}, born ${birthdate}`);
+  }
+}
+
+knex.select().from("famous_people").where("last_name", name).orWhere("first_name", name).then(function (data) {
+  displayResult(data)
+}).finally(function() {
+  knex.destroy();
+});
